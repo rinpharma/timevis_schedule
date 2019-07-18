c0f46a7d51fdf0dc7c2ed189e2098a0d2e564faf
James Black <james.black.jb2@roche.com>
2019-07-18 09:29:11 +0200

    down to core

--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@ library(timevis)
 source("ui-helpers.R")
 
 fluidPage(
-  title = "timevis - An R package for creating timeline visualizations",
+  title = "R/Pharma 2019 schedule",
   tags$head(
     tags$link(href = "style.css", rel = "stylesheet"),
 
@@ -25,131 +25,26 @@ fluidPage(
     tags$meta(name = "twitter:description", content = share$description),
     tags$meta(name = "twitter:image", content = share$image)
   ),
-  tags$a(
-    href="https://github.com/daattali/timevis",
-    tags$img(style="position: absolute; top: 0; right: 0; border: 0;",
-             src="github-orange-right.png",
-             alt="Fork me on GitHub")
-  ),
   div(id = "header",
     div(id = "title",
-      "timevis"
+      "R/Pharma 2019 schedule"
     ),
     div(id = "subtitle",
-        "An R package for creating timeline visualizations"),
+        "DRAFT - Subject to change"),
     div(id = "subsubtitle",
-        "By",
+        "Code for this schedule forked from the timevis docs - a package by",
         tags$a(href = "http://deanattali.com/", "Dean Attali"),
         HTML("&bull;"),
         "Available",
         tags$a(href = "https://github.com/daattali/timevis", "on GitHub"),
-        HTML("&bull;"),
-        tags$a(href = "http://daattali.com/shiny/", "More apps"), "by Dean"
+        HTML("&bull;")
     )
   ),
   tabsetPanel(
     id = "mainnav",
     tabPanel(
-      div(icon("calendar"), "Basic timeline"),
-      timevisOutput("timelineBasic"),
-      div(
-        id = "samplecode",
-        fluidRow(
-          column(
-            6,
-            div(class = "codeformat",
-              "In R console or R markdown documents"),
-            tags$pre(codeConsole)
-          ),
-          column(
-            6,
-            div(class = "codeformat",
-                "In Shiny apps"),
-            tags$pre(codeShiny)
-          )
-        )
-      )
-    ),
-
-    tabPanel(
-      div(icon("cog"), "Custom style"),
-      timevisOutput("timelineCustom")
-    ),
-
-    tabPanel(
-      div(icon("trophy"), "World Cup 2014"),
-      timevisOutput("timelineWC")
-    ),
-
-    tabPanel(
       div(icon("users"), "Groups"),
       timevisOutput("timelineGroups")
-    ),
-
-    tabPanel(
-      div(icon("sliders"), "Fully interactive"),
-      fluidRow(
-        column(
-          8,
-          fluidRow(column(12,
-            timevisOutput("timelineInteractive")
-          )),
-          fluidRow(
-            column(
-              12,
-              div(id = "interactiveActions",
-                  class = "optionsSection",
-                  tags$h4("Actions:"),
-                  actionButton("fit", "Fit all items"),
-                  actionButton("setWindowAnim", "Set window 2016-01-07 to 2016-01-25"),
-                  actionButton("setWindowNoAnim", "Set window without animation"),
-                  actionButton("center", "Center around 2016-01-23"),
-                  actionButton("focus2", "Focus item 4"),
-                  actionButton("focusSelection", "Focus current selection"),
-                  actionButton("addTime", "Add a draggable vertical bar 2016-01-17")
-              )
-            )
-          ),
-          fluidRow(
-            column(
-              4,
-              div(class = "optionsSection",
-                  uiOutput("selectIdsOutput", inline = TRUE),
-                  actionButton("selectItems", "Select"),
-                  checkboxInput("selectFocus", "Focus on selection", FALSE)
-              )
-            ),
-            column(
-              4,
-              div(class = "optionsSection",
-                  textInput("addText", tags$h4("Add item:"), "New item"),
-                  dateInput("addDate", NULL, "2016-01-15"),
-                  actionButton("addBtn", "Add")
-              )
-            ),
-            column(
-              4,
-              div(class = "optionsSection",
-                  uiOutput("removeIdsOutput", inline = TRUE),
-                  actionButton("removeItem", "Remove")
-              )
-            )
-          )
-        ),
-        column(4,
-           div(
-             id = "timelinedata",
-             class = "optionsSection",
-             tags$h4("Data:"),
-             tableOutput("table"),
-             hr(),
-             div(tags$strong("Visible window:"),
-                 textOutput("window", inline = TRUE)),
-             div(tags$strong("Selected items:"),
-                 textOutput("selected", inline = TRUE))
-           )
-        )
-      )
     ),
     tabPanel(
       div(icon("question"), "Usage"),
